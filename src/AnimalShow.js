ac2fe400377ae668a1b090fc359fcd55e0914c81
Esther <esther.mothanaprakoon@gmail.com>
2024-11-06 09:03:52 +0700

    [add] useState and images

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,6 +1,41 @@
-function AnimalShow () {
-    console.log("yes");
-    
+import { useState } from "react";
+import "./AnimalShow.css"
+import bird from "./svg/bird.svg";
+import dog from "./svg/dog.svg";
+import cat from "./svg/cat.svg"; 
+import cow from "./svg/cow.svg";
+import gator from "./svg/gator.svg";
+import horse from "./svg/horse.svg";
+import heart from "./svg/heart.svg";
+
+//dog.svg doesn't display
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
+function AnimalShow({ type }) {
+  const [clicks, setClicks] = useState(0);
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
+  return (
+    <div className="animal-show" onClick={handleClick}>
+      <img src={svgMap[type]} alt="animal" className="animal" />
+      <img 
+      src={heart} 
+      alt="heart" 
+      style={{ width: 10 + 10 * clicks }}
+      className="heart"
+       />
+    </div>
+  );
 }
 
-export {AnimalShow}
+export { AnimalShow };
--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-// import { AnimalShow } from "./AnimalShow"
+import { AnimalShow } from "./AnimalShow"
 import { useState } from "react"
+import './App.css'
+
+function getRandomAnimal () {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse']   
+  return animals[Math.floor(Math.random()* animals.length)]
+}
 
 function App () {
-  const [count, setCount] = useState(0)  
+  const [animals, setAnimals] = useState([])
+
   const handleClick = () => {
-    setCount(count + 10)
+    setAnimals([...animals, getRandomAnimal()]) 
   }  
 
-  return (<div>
+  const renderAnimals = animals.map((animal, index) => {    
+    return <AnimalShow type={animal} key={index} />
+  })
+  
+  return (
+  <div className='app'>
     <button onClick={handleClick}>Add Animal</button>
-    <div>Count is {count}</div>
+    <div className='animal-list'>{renderAnimals}</div>
   </div>)
 }
 
